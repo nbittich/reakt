8792a160b3ed938d487085d1f696563ebe77213a
Nordine Bittich <3816305+nbittich@users.noreply.github.com>
2024-02-18 13:59:24 +0100

    Update counter-manual.js
    delete button

--- counter-manual.js
+++ counter-manual.js
@@ -6,13 +6,15 @@ export function CounterManualComponent(getCount, setCount) {
   div.innerHTML = `
        <button>incr</button>
        <button>decr</button>
+       <button>del</button>
        <p></p>
 `;
-  const [buttonIncr, buttonDecr] = div.querySelectorAll("button");
+  const [buttonIncr, buttonDecr, buttonDel] = div.querySelectorAll("button");
   const p = div.querySelector("p");
 
   buttonIncr.onclick = () => setCount(getCount() + 1);
   buttonDecr.onclick = () => setCount(getCount() - 1);
+  buttonDel.onclick  = () => Reakt.destroyComponent(div); 
 
   Reakt.registerComponent(div, getCount, [
     () => (p.innerText = `Count: ${getCount()}`),
